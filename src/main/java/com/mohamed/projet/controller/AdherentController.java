8f9d763e942dbaa73b576e5b48f95936b9d87c05
Mohamed-El-amri2000 <112402264+Mohamed-El-amri2000@users.noreply.github.com>
2022-09-16 19:00:40 +0200

    Update AdherentController.java
    solve some problems

--- src/main/java/com/mohamed/projet/controller/AdherentController.java
+++ src/main/java/com/mohamed/projet/controller/AdherentController.java
@@ -51,6 +51,13 @@ public String getAllAdherent(Model model,
 
 @GetMapping("delete")
 public String deleteAdh(Model model,Long id_adherent,int page,int size,String cin) {
+        Adherent adherent2 = adherentService.getAdherentById(id_adherent);
+	Page<Emprunt> pages= empruntService.consulterEmprunt(adherent2,0,5);
+	if (pages != null){
+		for(Emprunt e: pages){
+			empruntService.deleteEmp(e.getId_emprunt());
+		}
+	}
 	adherentService.deleteAdh(id_adherent);
 	
 	return "redirect:/adherent/lister?page="+page+"&size="+size+"&cin="+cin;
